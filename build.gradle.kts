858fa1e659057ae7dc6bc1e7e2dab5d94fffaff7
AleksandreBakidze <aleksandrebakidze@gmail.com>
2023-02-18 23:53:23 +0400

    added dependencies and create data in firebase (songs list)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ buildscript{
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.28.3-alpha")
+        classpath("com.google.gms:google-services:4.3.15")
     }
 }
 
